c0f47b2539271cafe3e0692c5c1229d25897e498
rmitnl <rmartin@Zenbook23>
2024-04-30 10:15:55 -0400

    test

---
+++ json-test/json-test.py
@@ -0,0 +1,8 @@
+import json
+
+x = '{ "key1":"value1", "key2":"value2", "key3":"value3", "key4":"value4", "key5":"value5" }'
+
+jsonx = json.loads(x)
+number = len(jsonx)
+print(json.dumps(jsonx, indent=3))
+print(number)
