5e0c92667d7b18118514a73d787de203059343c7
rmitnl <rmartin@Zenbook23>
2024-05-08 15:17:35 -0400

    updated

--- json-test/json-test.py
+++ json-test/json-test.py
@@ -1,7 +1,7 @@
 import json
 
-x = '{ "key2":"value2", "key3":"value3", "key4":"value4", "key5":"value5" }'
-#x = '{ "key1":"value1", "key2":"value2", "key3":"value3", "key4":"value4", "key5":"value5" }'
+#x = '{ "key2":"value2", "key3":"value3", "key4":"value4", "key5":"value5" }'
+x = '{ "key1":"value1", "key2":"value2", "key3":"value3", "key4":"value4", "key5":"value5" }'
 
 jsonx = json.loads(x)
 number = len(jsonx)
