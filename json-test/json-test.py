789c6cb9e83faf5c98b945d8330036ae73b98e25
rmitnl <rmartin@Zenbook23>
2024-05-23 12:25:17 -0400

    test

--- json-test/json-test.py
+++ json-test/json-test.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 #x = '{ "key2":"value2", "key3":"value3", "key4":"value4", "key5":"value5" }'
 x = '{ "key1":"value1", "key2":"value2", "key3":"value3", "key4":"value4", "key5":"value5" }'
@@ -6,4 +7,5 @@ x = '{ "key1":"value1", "key2":"value2", "key3":"value3", "key4":"value4", "key5
 jsonx = json.loads(x)
 number = len(jsonx)
 print(json.dumps(jsonx, indent=3))
+print(sys.argv[1:])
 #print(number)
